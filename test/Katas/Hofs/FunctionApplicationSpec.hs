c4b15236a3bb08c976079137c22a3535bb281d8e
Attila Domokos <adomokos@gmail.com>
2018-02-05 22:15:50 -0600

    Add solution for function application spec

--- test/Katas/Hofs/FunctionApplicationSpec.hs
+++ test/Katas/Hofs/FunctionApplicationSpec.hs
@@ -3,14 +3,18 @@ module Katas.Hofs.FunctionApplicationSpec (spec) where
 import Test.Hspec
 import Test.QuickCheck
 
+main :: IO ()
+main = hspec spec
+
 spec :: Spec
 spec = do
     describe "Function application with $" $ do
         it "let's get rid of parens" $ do
-            pending
             {- double the list of 1-6 and sum its values -}
-            41 `shouldBe` 42
+            (sum $ map (*2) [1..6])  `shouldBe` 42
             {- sqrt of 3 + 4 + 9 -}
-            3 `shouldBe` 4
+            (sqrt $ foldl1 (+) [3,4,9]) `shouldBe` 4
             {- double 1-5, sum of elements greater than 5 -}
-            5 `shouldBe` 24
+            (sum $ filter (>5) $ map (*2) [1..5]) `shouldBe` 24
+
+-- Continue to Katas.Hofs.FunctionCompositionSpec
