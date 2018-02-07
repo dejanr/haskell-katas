5ec59782906df994f54854c205464b6e3d4bb6c5
Attila Domokos <adomokos@gmail.com>
2018-02-07 17:15:09 -0600

    Add solutions for recursive data types

--- test/Katas/Types/RecursiveDataStructuresSpec.hs
+++ test/Katas/Types/RecursiveDataStructuresSpec.hs
@@ -3,33 +3,56 @@ module Katas.Types.RecursiveDataStructuresSpec (spec) where
 import Test.Hspec
 import Test.QuickCheck
 
+main :: IO ()
+main = hspec spec
+
 {- Example 1 -}
 {- Define a list that uses Cons to add elements to the list -}
+data List a = Empty | Cons a (List a) deriving (Show, Read, Eq, Ord)
+-- Might be easier to understand it with the Record syntax
+-- data List a = Empty | Cons { listHead :: a, listTail :: List a} deriving (Show, Read, Eq, Ord) 
+
 {- Use infixr 5 -}
-{- data List a = ___ -}
+infixr 5 :-:
+
+data List' a = Empty' | a :-: (List' a) deriving (Show, Read, Eq, Ord)
 
 {- Define a Tree structure with EmptyTree and Node  -}
-{- data Tree a = ___ -}
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show, Eq, Ord)
 
-{- treeInsert :: (Ord a) => a -> Tree a -> Tree a -}
+singleton :: a -> Tree a
+singleton x = Node x EmptyTree EmptyTree
 
-{- treeElem :: (Ord a) => a -> Tree a -> Bool -}
+treeInsert :: (Ord a) => a -> Tree a -> Tree a
+treeInsert x EmptyTree = singleton x
+treeInsert x (Node y left right)
+    | x == y = Node x left right
+    | x < y = Node y (treeInsert x left) right
+    | otherwise = Node y left (treeInsert x right)
+
+treeElem :: (Ord a) => a -> Tree a -> Bool
+treeElem x EmptyTree = False
+treeElem x (Node y left right)
+    | x == y = True
+    | x < y = treeElem x left
+    | otherwise = treeElem x right
+
 
 spec :: Spec
 spec = do
     describe "Recursive Data Structures" $ do
         {- Example 1 -}
-        {- it "can define a List" $ do -}
-            {- (5 `Cons` Empty) `shouldBe` Cons 5 Empty -}
-            {- 4 `Cons` (5 `Cons` Empty) -}
-                {- `shouldBe` Cons 4 (Cons 5 Empty) -}
+        it "can define a List" $ do
+            (5 `Cons` Empty) `shouldBe` Cons 5 Empty
+            4 `Cons` (5 `Cons` Empty)
+                `shouldBe` Cons 4 (Cons 5 Empty)
         it "can set fixity declarations with infixr 5" $ do
-            pending
-            {- 5 :-: Empty `shouldBe` ((:-:) 5 Empty) -}
-            {- 4 :-: 5 :-: Empty `shouldBe` ((:-:) 4 ((:-:) 5 Empty)) -}
+            5 :-: Empty' `shouldBe` ((:-:) 5 Empty')
+            4 :-: 5 :-: Empty' `shouldBe` ((:-:) 4 ((:-:) 5 Empty'))
         it "can express a binary search tree" $ do
-            pending
-            {- let nums = [8,6,4,1,7,3,5] -}
-            {- let numsTree = foldr treeInsert EmptyTree nums -}
-            {- treeElem 5 numsTree `shouldBe` True -}
-            {- treeElem 2 numsTree `shouldBe` False -}
+            let nums = [8,6,4,1,7,3,5]
+            let numsTree = foldr treeInsert EmptyTree nums
+            treeElem 5 numsTree `shouldBe` True
+            treeElem 2 numsTree `shouldBe` False
+
+-- Continue to Katas.Types.MoreTypeClassesSpec
