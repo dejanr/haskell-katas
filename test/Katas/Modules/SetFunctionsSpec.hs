d92332a37c0769688772d4404a9d9c3abbaafca7
Attila Domokos <adomokos@gmail.com>
2018-02-06 18:09:23 -0600

    Solve the Data.Set related spec

--- test/Katas/Modules/SetFunctionsSpec.hs
+++ test/Katas/Modules/SetFunctionsSpec.hs
@@ -4,6 +4,9 @@ import Test.Hspec
 import Test.QuickCheck
 import qualified Data.Set as Set
 
+main :: IO ()
+main = hspec spec
+
 text1 = "I just had an anime dream. Anime... Reality... Are they so different?"
 text2 = "The old man left his garbage can out and now his trash is all over my lawn!"
 set1 = Set.fromList text1
@@ -13,35 +16,31 @@ spec :: Spec
 spec = do
     describe "Set" $ do
         it "can convert List to Set" $ do
-            pending
-            {- ___ `shouldBe` ___ " .?AIRadefhijlmnorstuy" -}
-            {- ___ `shouldBe` ___ " !Tabcdefglhilmnorstuvwy" -}
+            set1 `shouldBe` Set.fromList " .?AIRadefhijlmnorstuy"
+            set2 `shouldBe` Set.fromList " !Tabcdefglhilmnorstuvwy"
         it "can find the intersection" $ do
-            pending
-            {- ___ set1 set2 -}
-                {- `shouldBe` Set.fromList " adefhilmnorstuy" -}
+            Set.intersection  set1 set2
+                `shouldBe` Set.fromList " adefhilmnorstuy"
         it "can find the difference" $ do
-            pending
-            {- ___ set1 set2 -}
-                {- `shouldBe` Set.fromList ".?AIRj" -}
-            {- ___ set2 set1 -}
-                {- `shouldBe` Set.fromList "!Tbcgvw" -}
+            Set.difference set1 set2
+                `shouldBe` Set.fromList ".?AIRj"
+            Set.difference set2 set1
+                `shouldBe` Set.fromList "!Tbcgvw"
         it "can calculate the union" $ do
-            pending
-            {- ___ set1 set2 -}
-                {- `shouldBe` Set.fromList " !.?AIRTabcdefghijlmnorstuvwy" -}
+            Set.union set1 set2
+                `shouldBe` Set.fromList " !.?AIRTabcdefghijlmnorstuvwy"
         it "can use null, size, singleton, insert, delete" $ do
-            pending
-            {- ___ Set.empty `shouldBe` True -}
-            {- ___ (Set.fromList [1,2,3]) `shouldBe` False -}
-            {- ___ Set.empty `shouldBe` 0 -}
-            {- ___ (Set.fromList [1,2,3]) `shouldBe` 3 -}
-            {- ___ 9 `shouldBe` Set.fromList [9] -}
-            {- (___ 8 $ Set.fromList [1..4]) -}
-                {- `shouldBe` Set.fromList [1,2,3,4,8] -}
-            {- (___ 4 $ Set.fromList [3,4,5,4,3,4,5]) -}
-                {- `shouldBe` Set.fromList [3,5] -}
+            Set.null Set.empty `shouldBe` True
+            Set.null (Set.fromList [1,2,3]) `shouldBe` False
+            Set.size Set.empty `shouldBe` 0
+            Set.size (Set.fromList [1,2,3]) `shouldBe` 3
+            Set.singleton 9 `shouldBe` Set.fromList [9]
+            (Set.insert 8 $ Set.fromList [1..4])
+                `shouldBe` Set.fromList [1,2,3,4,8]
+            (Set.delete 4 $ Set.fromList [3,4,5,4,3,4,5])
+                `shouldBe` Set.fromList [3,5]
         it "can filter odd items from set" $ do
-            pending
-            {- (___ ___ $ Set.fromList [3,4,5,6,7,2,3,4]) -}
-                {- `shouldBe` Set.fromList [3,5,7] -}
+            (Set.filter odd $ Set.fromList [3,4,5,6,7,2,3,4])
+                `shouldBe` Set.fromList [3,5,7]
+
+-- Continue to Katas.Types.MakingTypesSpec
