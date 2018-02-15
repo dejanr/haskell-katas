d9227c4ccf80b008b8ef69fd5bdffdae5106ee3d
Attila Domokos <adomokos@gmail.com>
2018-02-15 09:59:20 +0100

    Solve the basic RPN Calculator

--- test/Katas/Exercises/RPNCalculatorSpec.hs
+++ test/Katas/Exercises/RPNCalculatorSpec.hs
@@ -3,23 +3,27 @@ module Katas.Exercises.RPNCalculatorSpec (spec) where
 import Test.Hspec
 import Test.QuickCheck
 
-{- foldingFunction :: (Num a, Read a) => [a] -> String -> [a] -}
+main :: IO ()
+main = hspec spec
 
-{- solveRPN :: (Num a, Read a) => String -> a -}
+foldingFunction :: (Num a, Read a) => [a] -> String -> [a]
+foldingFunction (x:y:xs) "+" = (x + y) : xs
+foldingFunction (x:y:xs) "*" = (x * y) : xs
+foldingFunction (x:y:xs) "-" = (y - x) : xs
+foldingFunction xs x = (read x) : xs
+
+solveRPN :: (Num a, Read a) => String -> a
+solveRPN input = head $ foldl foldingFunction [] $ words input
 
 spec :: Spec
 spec = do
     describe "reverse polish notation" $ do
         it "uses a foldingFunction" $ do
-            pending
-            {- foldl foldingFunction [] ["1","2","+"] `shouldBe` [3] -}
-            {- foldl foldingFunction [] ["2","3","*"] `shouldBe` [6] -}
+            foldl foldingFunction [] ["1","2","+"] `shouldBe` [3]
+            foldl foldingFunction [] ["2","3","*"] `shouldBe` [6]
         it "calculates simple addition" $ do
-            pending
-            {- solveRPN "1 2 +" `shouldBe` 3 -}
+            solveRPN "1 2 +" `shouldBe` 3
         it "calculates simple multiplication" $ do
-            pending
-            {- solveRPN "2 3 *" `shouldBe` 6 -}
+            solveRPN "2 3 *" `shouldBe` 6
         it "calculates more complex expressions" $ do
-            pending
-            {- solveRPN "10 4 3 + 2 * -" `shouldBe` (-4) -}
+            solveRPN "10 4 3 + 2 * -" `shouldBe` (-4)
