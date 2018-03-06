1d1e7956ce66881bc9cbd473d0f9ef0323e346fa
Attila Domokos <adomokos@gmail.com>
2018-03-06 08:46:28 -0600

    Add solution for ordering monoid spec

--- test/Katas/Fa/OrderingMonoidsSpec.hs
+++ test/Katas/Fa/OrderingMonoidsSpec.hs
@@ -4,6 +4,9 @@ import Test.Hspec
 import Test.QuickCheck
 import Data.Monoid
 
+main :: IO ()
+main = hspec spec
+
 {-
     A practical example of where monoids could be useful
 -}
@@ -12,28 +15,35 @@ import Data.Monoid
   Create a function (without monoids) where first the length is compared,
   and if they are equal, then the words are compared
 -}
--- lengthCompareVerbose :: String -> String -> Ordering
+lengthCompareVerbose :: String -> String -> Ordering
+lengthCompareVerbose x y =
+    let a = length x `compare` length y
+        b = x `compare` y
+    in if a == EQ then b else a
 
 -- Use the same logic, but use monoids
-{- lengthCompare :: String -> String -> Ordering -}
+lengthCompare :: String -> String -> Ordering
+lengthCompare x y = (length x `compare` length y) `mappend`
+                    (x `compare` y)
 
 
 -- Compare length, vowels (aeiou) and strings in this order
--- lengthVowelCompare :: String -> String -> Ordering
+lengthVowelCompare :: String -> String -> Ordering
+lengthVowelCompare x y = (length x `compare` length y) `mappend`
+                         (vowels  x `compare` vowels y) `mappend`
+                         (x `compare` y)
+                             where vowels = length . filter (`elem` "aeiou")
 
 spec :: Spec
-spec = do
+spec =
     describe "Monoids" $ do
         it "verbose lengthcompare" $ do
-            pending
-            {- lengthCompareVerbose "zen" "ants" `shouldBe` LT -}
-            {- lengthCompareVerbose "zen" "ant" `shouldBe` GT -}
+            lengthCompareVerbose "zen" "ants" `shouldBe` LT
+            lengthCompareVerbose "zen" "ant" `shouldBe` GT
         it "lengthcompare" $ do
-            pending
-            {- lengthCompare "zen" "ants" `shouldBe` LT -}
-            {- lengthCompare "zen" "ant" `shouldBe` GT -}
+            lengthCompare "zen" "ants" `shouldBe` LT
+            lengthCompare "zen" "ant" `shouldBe` GT
         it "can compare length and vowels" $ do
-            pending
-            {- lengthVowelCompare "zen" "anna" `shouldBe` LT -}
-            {- lengthVowelCompare "zen" "ana" `shouldBe` LT -}
-            {- lengthVowelCompare "zen" "ann" `shouldBe` GT -}
+            lengthVowelCompare "zen" "anna" `shouldBe` LT
+            lengthVowelCompare "zen" "ana" `shouldBe` LT
+            lengthVowelCompare "zen" "ann" `shouldBe` GT
