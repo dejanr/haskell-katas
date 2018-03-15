443dde27ab9862b3227aa77072b91937b25be551
Attila Domokos <adomokos@gmail.com>
2018-03-15 09:56:08 -0500

    Add the solutions for the do notation example

--- test/Katas/Monads/DoNotationSpec.hs
+++ test/Katas/Monads/DoNotationSpec.hs
@@ -3,39 +3,42 @@ module Katas.Monads.DoNotationSpec (spec) where
 import Test.Hspec
 import Test.QuickCheck
 
-
-{- foo :: Maybe String -}
-{- foo = ___   >>= (\x -> -}
-      {- ___ >>= (\y -> -}
-      {- ___ )) -}
+foo :: Maybe String
+foo = Just 3   >>= (\x ->
+      Just "!" >>= (\y ->
+      Just (show x ++ y)))
 
 -- Use do expression
-{- fooMonad :: Maybe String -}
+fooMonad :: Maybe String
+fooMonad = do
+    let x = 3
+        y = "!"
+    Just (show x ++ y)
 
 -- Use do expression here as well
-{- marySue :: Maybe Bool -}
+marySue :: Maybe Bool
+marySue = do
+    x <- Just 9
+    Just (x > 8)
 
 main :: IO ()
 main = hspec spec
 
 spec :: Spec
-spec = do
+spec =
     describe "The do notation" $ do
         it "can combine functions with Maybe values" $ do
-            pending
             -- Use a lambda here
-            {- (Just 3 >>= (___)) -}
-                {- `shouldBe` Just "3!" -}
-            {- (Just 3 >>= (\x -> ___  >>= (\y -> Just ___))) -}
-                {- `shouldBe` Just "3!" -}
-            {- (Just 3 >>= (\x -> Just "!" >>= (\y -> ___))) -}
-                {- `shouldBe` (Nothing :: Maybe String) -}
+            (Just 3 >>= (\x -> Just (show x ++ "!")))
+                `shouldBe` Just "3!"
+            (Just 3 >>= (\x -> Just "!" >>= (\y -> Just (show x ++ y))))
+                `shouldBe` Just "3!"
+            (Just 3 >>= (\x -> Just "!" >>= const Nothing))
+                `shouldBe` (Nothing :: Maybe String)
         it "can be wrapped on different lines" $ do
-            pending
-            {- foo `shouldBe` Just "3!" -}
-            {- fooMonad `shouldBe` Just "3!" -}
+            foo `shouldBe` Just "3!"
+            fooMonad `shouldBe` Just "3!"
         it "can simplify more complex expressions" $ do
-            pending
-            -- Use a lambda here for the prdicate
-            {- (Just 9 >>= (___)) `shouldBe` Just True -}
-            {- marySue `shouldBe` Just True -}
+            -- Use a lambda here for the predicate
+            (Just 9 >>= (\x -> Just (x > 8))) `shouldBe` Just True
+            marySue `shouldBe` Just True
