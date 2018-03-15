91f513d59d4ae2e92c49b9d9fecf64df4cf4ba51
Attila Domokos <adomokos@gmail.com>
2018-03-15 08:38:10 -0500

    Make the Monads IntroSpec pass

--- test/Katas/Monads/IntroSpec.hs
+++ test/Katas/Monads/IntroSpec.hs
@@ -16,39 +16,37 @@ import Test.Hspec
 import Test.QuickCheck
 
 -- Let's not use >>=, create an applyMaybe fn
-{- applyMaybe :: Maybe a -> (a -> Maybe b) -> Maybe b -}
+applyMaybe :: Maybe a -> (a -> Maybe b) -> Maybe b
+applyMaybe Nothing _ = Nothing
+applyMaybe (Just x) f = f x
 
 main :: IO ()
 main = hspec spec
 
 spec :: Spec
-spec = do
+spec =
     describe "Monads" $ do
         it "can wrap a normal value into a Just value" $ do
-            pending
             -- Use a lambda here
-            {- (___) 1 `shouldBe` Just 2 -}
-            {- (___) 100 `shouldBe` Just 101 -}
+            (\x -> Just (x + 1)) 1 `shouldBe` Just 2
+            (\x -> Just (x + 1)) 100 `shouldBe` Just 101
         it "can use applyMaybe function to Maybe values" $ do
-            pending
-            {- (Just 3 `applyMaybe` \x -> Just (x+1)) -}
-                {- `shouldBe` Just 4 -}
-            {- (Just "smile" `applyMaybe` \x -> Just (x ++ " :)")) -}
-                {- `shouldBe` Just "smile :)" -}
-            {- (Nothing `applyMaybe` \x -> Just (x+1)) -}
-                {- `shouldBe` Nothing -}
-            {- (Nothing `applyMaybe` \x -> Just (x ++ " :)")) -}
-                {- `shouldBe` Nothing -}
+            (Just 3 `applyMaybe` \x -> Just (x+1))
+                `shouldBe` Just 4
+            (Just "smile" `applyMaybe` \x -> Just (x ++ " :)"))
+                `shouldBe` Just "smile :)"
+            (Nothing `applyMaybe` \x -> Just (x+1))
+                `shouldBe` Nothing
+            (Nothing `applyMaybe` \x -> Just (x ++ " :)"))
+                `shouldBe` Nothing
         it "checks if value is >2" $ do
-            pending
-            {- (Just 3 `applyMaybe` \x -> if x > 2 then Just x else Nothing) -}
-                {- `shouldBe` Just 3 -}
-            {- (Nothing `applyMaybe` \x -> if x > 2 then Just x else Nothing) -}
-                {- `shouldBe` Nothing -}
+            (Just 3 `applyMaybe` \x -> if x > 2 then Just x else Nothing)
+                `shouldBe` Just 3
+            (Nothing `applyMaybe` \x -> if x > 2 then Just x else Nothing)
+                `shouldBe` Nothing
         it "works like our applyMaybe fn" $ do
-            pending
-            {- let value = return "WHAT" :: Maybe String -}
-            {- value `shouldBe` ___ -}
+            let value = return "WHAT" :: Maybe String
+            value `shouldBe` Just "WHAT"
             -- Use a lambda here
-            {- (Just 9 >>= ___) `shouldBe` Just 90 -}
-            {- (Nothing >>= ___) `shouldBe` Nothing -}
+            (Just 9 >>= \x -> Just (10 * x)) `shouldBe` Just 90
+            (Nothing >>= \x -> Just (10 * x)) `shouldBe` Nothing
