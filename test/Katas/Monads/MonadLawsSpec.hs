7891a5f41fd5e2f8575e6dbc9b1db0a9b0a6a28e
Attila Domokos <adomokos@gmail.com>
2018-03-19 10:48:10 -0500

    Solve the Monad Laws spec

--- test/Katas/Monads/MonadLawsSpec.hs
+++ test/Katas/Monads/MonadLawsSpec.hs
@@ -11,12 +11,21 @@ import Control.Monad
 
        Take a value, put it in the default context with return
        and then feed to a function by using `>>=`, it's the same
-       as just taking the value an applying the function to it.
+       as just taking the value and applying the function to it.
        return x >>= f is the same as f x
+
+       λ> return 3 >>= (\x -> return (x*2))
+       6
+       λ> Just 2 >>= (\x -> return (x*2))
+       Just 4
 
        For the list monad, return puts something in a singleton list,
        the >>= implementation for lists goes over all the values
        in the list and applies the function to them.
+
+       λ> [1,2,3] >>= (\x -> [x*3])
+       [3,6,9]
+
 
     2. Right Identity
 
@@ -24,11 +33,20 @@ import Control.Monad
        then the result is our original monadic value.
        m >>= return is no different than just m
 
+       λ> Just 4 >>= (\x -> return x)
+       Just 4
+
     3. Associativity
 
        When we have a chain of monadic function application with
        >>=, it shouldn't matter how they're nested.
        (m >>= f) >>= g is just like doing m >>= (\x -> f x >> g)
+
+       λ> Just 2 >>= (\x -> return (x * 2) >>= (\y -> return (y * 3)))
+       Just 12
+
+       λ> (Just 2 >>= (\x -> return (x * 2))) >>= (\y -> return (y * 3))
+       Just 12
 -}
 
 type Birds = Int
@@ -49,26 +67,22 @@ main :: IO ()
 main = hspec spec
 
 spec :: Spec
-spec = do
+spec =
     describe "Monad Laws" $ do
-        it "abides by the first law - left identity" $ do
-            pending
-            {- (return 3 >>= (___)) -}
-                {- `shouldBe` ((\x -> Just (x+1000)) 3) -}
-        it "holds true with lists" $ do
-            pending
-            {- (return "WoM" >>= ___) -}
-                {- `shouldBe` ((\x -> [x,x,x]) "WoM") -}
+        it "abides by the first law - left identity" $
+            (return 3 >>= \x -> Just (x+1000))
+                 `shouldBe` (\x -> Just (x+1000)) 3
+        it "holds true with lists" $
+            (return "WoM" >>= \x -> [x,x,x])
+                `shouldBe` (\x -> [x,x,x]) "WoM"
         it "abides by the second law - right identity" $ do
-            pending
-            {- (___ >>= (\x -> return x)) -}
-                {- `shouldBe` (Just "move on up") -}
-            {- ([1,2,3,4] >>= ___) -}
-                {- `shouldBe` [1,2,3,4] -}
+            (return "move on up" >>= return)
+                `shouldBe` Just "move on up"
+            ([1,2,3,4] >>= return)
+                `shouldBe` [1,2,3,4]
         it "abides by the third law - associativity" $ do
-            pending
             -- It lands 2 on the right, then 2 on the left and again 2 on the right
-            -- ___
-                {- `shouldBe` Just (2,4) -}
-            {- (((return (0,0) >>= ___) >>= ___) >>= ___) -}
-                {- `shouldBe` Just (2,4) -}
+            (return (0,0) >>= landRight 2 >>= landLeft 2 >>= landRight 2)
+                `shouldBe` Just (2,4)
+            (((return (0,0) >>= landRight 2) >>= landLeft 2) >>= landRight 2)
+                `shouldBe` Just (2,4)
