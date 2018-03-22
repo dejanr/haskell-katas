a34702012e0dd8bb598e8d52e47b7aa7f2ebe80c
Attila Domokos <adomokos@gmail.com>
2018-03-21 21:36:49 -0500

    Add solution for Logging with Writer

--- test/Katas/Monads/LoggingWithWriterSpec.hs
+++ test/Katas/Monads/LoggingWithWriterSpec.hs
@@ -6,42 +6,60 @@ import Control.Monad.Writer
 import Data.Monoid
 
 -- Greatest common deviser
-{- gcd' :: Int -> Int -> Int -}
+gcd' :: Int -> Int -> Int
+gcd' a b
+    | b == 0 = a
+    | otherwise = gcd' b (a `mod` b)
 
-{- gcdWithLog :: Int -> Int -> Writer [String] Int -}
+gcdWithLog :: Int -> Int -> Writer [String] Int
+gcdWithLog a b
+    | b == 0 = do
+        tell ["Finished with " ++ show a]
+        return a
+    | otherwise = do
+        tell [show a ++ " mod " ++ show b ++ " = " ++ show (a `mod` b)]
+        gcdWithLog b (a `mod` b)
 
-{- newtype DiffList a = DiffList { getDiffList :: [a] -> [a] } -}
+newtype DiffList a = DiffList { getDiffList :: [a] -> [a] }
 
-{- toDiffList :: [a] -> DiffList a -}
+toDiffList :: [a] -> DiffList a
+toDiffList xs = DiffList (xs++)
 
-{- fromDiffList :: DiffList a -> [a] -}
+fromDiffList :: DiffList a -> [a]
+fromDiffList (DiffList f) = f []
 
-{- instance Monoid (DiffList a) where -}
+instance Monoid (DiffList a) where
+    mempty = DiffList (\xs -> [] ++ xs)
+    (DiffList f) `mappend` (DiffList g) = DiffList (f . g)
 
-{- gcdWithDiffList :: Int -> Int -> Writer (DiffList String) Int -}
+gcdWithDiffList :: Int -> Int -> Writer (DiffList String) Int
+gcdWithDiffList a b
+    | b == 0 = do
+        tell (toDiffList ["Finished with " ++ show a])
+        return a
+    | otherwise = do
+        result <- gcdWithDiffList b (a `mod` b)
+        tell (toDiffList [show a ++ " mod " ++ show b ++ " = " ++ show (a `mod` b)])
+        return result
 
 main :: IO ()
 main = hspec spec
 
 spec :: Spec
-spec = do
+spec =
     describe "Logging with Writer" $ do
-        it "can find the greatest common devisor" $ do
-            pending
-            {- gcd' 8 3 `shouldBe` 1 -}
+        it "can find the greatest common devisor" $
+            gcd' 8 3 `shouldBe` 1
         it "can decorate the gcd function with logging" $ do
-            pending
-            {- (fst $ runWriter $ gcdWithLog 8 3) `shouldBe` 1 -}
-            {- (snd $ runWriter $ gcdWithLog 8 3) -}
-                {- `shouldBe` ["8 mod 3 = 2","3 mod 2 = 1","2 mod 1 = 0","Finished with 1"] -}
-        it "can efficiently append to difference list" $ do
-            pending
-            {- (fromDiffList (toDiffList [1,2,3,4] `mappend` toDiffList [1,2,3])) -}
-                {- `shouldBe` [1,2,3,4,1,2,3] -}
-        it "can log with DiffList String" $ do
-            pending
-            {- (fromDiffList . snd . runWriter $ gcdWithDiffList 8 3) -}
-                {- `shouldBe` ["8 mod 3 = 2","3 mod 2 = 1","2 mod 1 = 0","Finished with 1"] -}
+            fst (runWriter $ gcdWithLog 8 3) `shouldBe` 1
+            snd (runWriter $ gcdWithLog 8 3)
+                `shouldBe` ["8 mod 3 = 2","3 mod 2 = 1","2 mod 1 = 0","Finished with 1"]
+        it "can efficiently append to difference list" $
+            fromDiffList (toDiffList [1,2,3,4] `mappend` toDiffList [1,2,3])
+                `shouldBe` [1,2,3,4,1,2,3]
+        it "can log with DiffList String" $
+            (fromDiffList . snd . runWriter $ gcdWithDiffList 8 3)
+                `shouldBe` ["Finished with 1","2 mod 1 = 0","3 mod 2 = 1","8 mod 3 = 2"]
 
 {-
     To run it in the RePL:
